a7c37689bad7d80c13c27c10e5900fe122a289a0
MinhTri0204 <tri.dominh0402@gmail.com>
2022-09-10 17:27:53 +0700

    upload lan 1

---
+++ Program/DoAn/DoAn/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DoAn.gui;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DoAn
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmConnectSystem());
+
+            //SqlConnection cnn = null;
+
+            //string sFilePath = Application.ExecutablePath + ".config";
+            //if (!System.IO.File.Exists(sFilePath))
+            //{
+            //    frmConnectSystem frmCnn = new frmConnectSystem();
+            //    if (frmCnn.ShowDialog() == DialogResult.Cancel)
+            //        Application.Exit();
+            //    else
+            //        Application.Restart();
+
+            //    return;
+            //}
+            //else
+            //{
+            //    string sCNN = ConfigurationManager.ConnectionStrings["DoAn.Properties.Settings.QL_BSachConnectionString"].ConnectionString;
+            //    cnn = new SqlConnection(sCNN);
+            //    try
+            //    {
+            //        cnn.Open();
+            //        cnn.Close();
+            //        frmDangNhap dn = new frmDangNhap();
+            //        dn.ShowDialog();
+            //    }
+            //    catch
+            //    {
+            //        frmConnectSystem frmCnn = new frmConnectSystem();
+            //        frmCnn.ShowDialog();
+            //    }
+            //}
+        }
+    }
+}
---
+++ Program/DoAn/DoAn/gui/frmConnectSystem.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DoAn.dao_xu_ly;
+using DoAn.connect_database;
+using DoAn.doi_tuong;
+using System.Data.SqlClient;
+
+namespace DoAn.gui
+{
+    public partial class frmConnectSystem : Form
+    {
+        AppSetting appset = new AppSetting();
+        SQLProvider db = new SQLProvider();
+        string connectionString = "";
+
+        public string ConnectionString { get => connectionString; set => connectionString = value; }
+
+        public frmConnectSystem()
+        {
+            InitializeComponent();
+            pnCN.BackColor = Color.FromArgb(100, 0, 0, 0);
+            lbSN.BackColor = Color.Transparent;
+            lbDN.BackColor = Color.Transparent;
+            lbUID.BackColor = Color.Transparent;
+            lbP.BackColor = Color.Transparent;
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            if (txtSN.Text == "" || txtDN.Text == "")
+                MessageBox.Show("Can not be empty!");
+            else
+            {
+                if (txtUID.Text == "")
+                    ConnectionString = string.Format("server={0};database={1};Integrated Security = true;", txtSN.Text, txtDN.Text);
+                else
+                    ConnectionString = string.Format("server={0};database={1};Integrated Security = true;uid={2};pwd={3}", txtSN.Text, txtDN.Text, txtUID.Text, txtP.Text);
+                
+                if (db.testConnection(ConnectionString))
+                    MessageBox.Show("Kết nối thành công !", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show("Kết nối KHÔNG thành công !", "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (ConnectionString != "")
+            {
+                appset.setConnectionString("DoAn.Properties.Settings.QL_BSachConnectionString", ConnectionString);
+                MessageBox.Show("Lưu kết nối thành công ! Hãy khởi động lại chương trình !", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                frmDangNhap dn = new frmDangNhap();
+                dn.ShowDialog();
+                this.Hide();
+            }
+        }
+    }
+}
